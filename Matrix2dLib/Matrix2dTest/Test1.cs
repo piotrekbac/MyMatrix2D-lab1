e4fd81190827e672bbd3df40450b4eddede27b7c
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-04-12 15:18:09 +0200

    commit 7

--- Matrix2dLib/Matrix2dTest/Test1.cs
+++ Matrix2dLib/Matrix2dTest/Test1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Matrix2dLib;
 using System;
+using Newtonsoft.Json.Bson;
 
 namespace Matrix2dTests
 {
@@ -27,6 +28,59 @@ namespace Matrix2dTests
 
             //Act i Assert
             Assert.AreEqual("[[0,0] , [0,0]]", matrixZero.ToString());
+        }
+
+        //Testujemy czy macierz poprawnie dodaje dwie macierze - operator dodawania
+        [TestMethod]
+        public void TestMatrixDodanieDwochMacierz()
+        {
+            //Arrange 
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(2, 0, 1, 2);
+
+            //Act
+            var MatrixSuma = m1 + m2;
+
+            //Assert
+            Assert.AreEqual("[[3,2] , [4,6]]", MatrixSuma.ToString());
+        }
+
+        //Testujemy domyślny konstruktor macierzy
+        public void TestMatrixDomyslnyKonstruktor()
+        {
+            //Arrange 
+            var domyslnyMatrix = new Matrix2d();
+
+            //Act i Assert
+            Assert.AreEqual("[1,0] , [0,1]", domyslnyMatrix.ToString());
+        }
+
+        //Testujemy czy macierz poprawnie odejmuje dwie macierze - operator odejmowanie 
+        public void TestMatrixOdejmowanie()
+        {
+            //Arrange 
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(2, 0, 1, 2);
+
+            //Act
+            var matrixRoznica = m1 - m2;
+
+            //Assert
+            Assert.AreEqual("[[-1,-2] , [2,-2]]", matrixRoznica.ToString());
+        }
+
+        //Testujemy czy macierz poprawnie mnoży dwie macierze - operator mnożenia
+        public void TestMatrixMnozenie()
+        {
+            //Arrange 
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(2, 0, 1, 2);
+
+            //Act
+            var matrixMnozenie = m1 * m2; 
+
+            //Assert
+            Assert.AreEqual("[[4,4] , [10,8]]", matrixMnozenie.ToString());
         }
 
 
