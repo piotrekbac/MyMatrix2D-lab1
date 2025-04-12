fe2241f52a0eb562a081cb7b471e223443d1193a
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-04-12 15:07:12 +0200

    commit 6

--- Matrix2dLib/Matrix2dTest/Test1.cs
+++ Matrix2dLib/Matrix2dTest/Test1.cs
@@ -7,6 +7,28 @@ namespace Matrix2dTests
     [TestClass]
     public class Matrix2dTests
     {
-        
+        //Testujemy czy macierz jest poprawnie inicjowana 
+        [TestMethod]
+        public void TestMatrixPoprawnaInicjowanie()
+        {
+            //Arrrange 
+            var matrixId = Matrix2d.Id;
+
+            //Act i Assert
+            Assert.AreEqual("[[1,0] , [0,1]]", matrixId.ToString());
+        }
+
+        //Testujemy czy macierz zerowa jest poprawnie inicjowana
+        [TestMethod]
+        public void TestMatrixZeroInicjowanie()
+        {
+            //Arrange 
+            var matrixZero = Matrix2d.Zero;
+
+            //Act i Assert
+            Assert.AreEqual("[[0,0] , [0,0]]", matrixZero.ToString());
+        }
+
+
     }
 }
